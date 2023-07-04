39b5e4194a11cbe7a659ef04e1a4e7f8186b071f
juanHernandezG <juan.hernandez1901@alumnos.biobio.cl>
2023-07-04 02:29:07 -0400

    commit components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { CarritoComprasComponent } from './pages/carrito-compras/carrito-compras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductoComponent,
+    CarritoComprasComponent
   ],
   imports: [
     BrowserModule
