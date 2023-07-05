be0438eaa9433a76c4c25db95c1806c06c43d0bd
Cristian <cristian.valenzuela1901@alumnos.ubiobio.cl>
2023-07-05 03:59:03 -0400

    v4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CarritoComprasComponent } from './pages/carrito-compras/carrito-compras.component';
+import { ProductossComponent } from './pages/productoss/productoss.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
 const routes: Routes = [
-  {path:'home', component: HomeComponent},
-  {path:'carrito', component: CarritoComprasComponent},
-  {path: '', redirectTo: 'home', pathMatch:'full'}
+  { path: 'home', component: HomeComponent },
+  { path: 'carrito', component: CarritoComprasComponent },
+  { path: 'productos', component: ProductossComponent },
+  { path: 'producto', component: ProductoComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
