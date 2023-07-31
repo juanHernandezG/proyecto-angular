ee8be5e3e92be35206a48c6c39467a76de5e836e
cristian23tbtb <cristian.valenzuela1901@alumnos.ubiobio.cl>
2023-07-31 03:16:26 -0400

    asfsa

--- src/app/pages/app.service.ts
+++ src/app/pages/app.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {map} from 'rxjs/operators';
@@ -131,4 +131,7 @@ export class AppService {
   agregarProd(producto:any): Observable<any>{
     return this.http.post<any>('http://localhost:3000/agregarprod',producto);
   }
+
+  
+
 }
